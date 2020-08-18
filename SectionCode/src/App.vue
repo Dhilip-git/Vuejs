2bbb2fdce3c9e0af6353f58bf86b9fc3ca1371cf
Dhilip Prasath R S <dhilipprasath@sedintechnologies.com>
2020-08-18 22:58:18 +0530

    Added section 11 scripts

---
+++ SectionCode/src/App.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="container">
+        <div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userdata.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userdata.pass">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control"
+                                v-model="userdata.age">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"
+                            v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"
+                                    v-model="sentmail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"
+                                    v-model="sentmail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"
+                                v-model="gender"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"
+                                v-model="gender"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control"
+                            v-model="selectedproperty">
+                        <option v-for="show in priority" >{{ show }}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <appSwitch :value="swithValue" :first="fName" :second="sName" :update="handleChange"></appSwitch>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userdata.email }}</p>
+                        <p>Password: {{ userdata.pass }}</p>
+                        <p>Age:{{ userdata.age }}</p>
+                        <p style="white-space:pre">Message: {{ message }}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li v-for="items in sentmail"> {{ items }}</li>
+                        </ul>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedproperty }}</p>
+                        <p>Switched:</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Switch from './Switch.vue';
+    
+    export default {
+        data(){
+            return{
+                fName:'Dhilip',
+                sName:'Prasath',
+                switched:true,
+                userdata: {
+                   email:'',
+                   pass:'',
+                   age:24
+                },
+                message:'welcome',
+                sentmail:[],
+                gender: 'male',
+                selectedproperty:'High',
+                priority:['High','Medium','Low']
+                
+            }
+        },
+        methods: {
+            
+             handleChange(value){
+                 console.log("value",value)
+                 this.switched = value;
+             }
+
+        },
+        components:{
+            appSwitch:Switch
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ SectionCode/src/Switch.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+    <p>{{ `MR ${first} ${second}` }}</p>
+        <button
+                id="on"
+                v-if="value"
+                @click="switched(value)"
+                :class="{active: value}">On</button>
+        <button
+        v-else
+                id="off"
+                @click="switched(value)"
+                :class="{active: !value}">Off</button>
+                 <p>test{{ value ? "true" : "false" }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['value','first','second', 'update'],
+        methods:{
+           switched(test){
+        this.update(!test)
+           }
+
+        }
+    }
+</script>
+
+<style scoped>
+    #on, #off {
+        width: 40px;
+        height: 20px;
+        background-color: lightgray;
+        padding: 2px;
+        display: inline-block;
+        margin: 10px -2px;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+</style>
